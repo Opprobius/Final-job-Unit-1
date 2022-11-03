d4ecc03fda8c3ba78c649124029d05d15252e0c7
Opprobius <98857147+Opprobius@users.noreply.github.com>
2022-11-03 18:44:30 -0500

    Opciones del menu
    link del api de referencia
    posible visualización del menú

--- tarea_part2.py
+++ tarea_part2.py
@@ -1,1 +1,17 @@
+import requests
 
+#https://pokeapi.co/api/v2
+#generation
+#pokemon-shape
+#ability
+#pokemon-habitat
+#type
+
+#funciones del menu
+menu_options = {
+        1: 'Listar pokemons por generación',
+        2: 'Listar pokemons por forma',
+        3: 'Listar pokemons por habilidad',
+        4: 'Listar pokemons por habitat.',
+        5: 'Listar pokemons por tipo',
+        6: 'Salir del programa.'
