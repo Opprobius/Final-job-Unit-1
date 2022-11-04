20d5f6be3d1749c38ee561781c0fbc5a7a6e53d3
FreddyxD5 <freddyxd5@gmail.com>
2022-11-04 13:17:06 -0500

    Añadido menu principal que sera usado para las acciones dentro del programa

--- tarea_part1.py
+++ tarea_part1.py
@@ -37,15 +37,15 @@ class Libro:
 
 #Metodos a realizar
 # 1 Leer archivo(Jesus)
-# 2 Listar libros
-# 3 Agregar libros
-# 4 Eliminar libros
-# 5 Buscar Libro por ISBN o titulo
-# 6 Ordenar libros por titulo
-# 7 Buscar libros por autor, editorial, y/o genero
-# 8 Buscar libros por numero de autores
-# 9 Editar Libro
-# 10 Guardar Libros
+# 2 Listar libros(Jefferson)
+# 3 Agregar libros(Jefferson)
+# 4 Eliminar libros(Jefferson o Jesus)
+# 5 Buscar Libro por ISBN o titulo (Jefferson o Jesus)
+# 6 Ordenar libros por titulo (Jefferson o Jesus)
+# 7 Buscar libros por autor, editorial, y/o genero (Jefferson o Jesus)
+# 8 Buscar libros por numero de autores (Jefferson o Jesus)
+# 9 Editar Libro (Jesus)
+# 10 Guardar Libros(Jesus)
 
 def cargar_archivo(nombre_archivo):
     try:
@@ -87,3 +87,77 @@ def guardar_datos(datos_libros, nombre_archivo):
     return ""
 
 guardar_datos(data, "test2.csv")
+
+menu_opciones = {
+    1: 'Cargar Archivo',
+    2: 'Listar Libros',
+    3: 'Agregar libro',
+    4: 'Eliminar libro',
+    5: 'Buscar Libro por ISBN o titulo',
+    6: 'Ordenar libros por titulo',
+    7: 'Buscar libros por autor, editorial, y/o genero',
+    8: 'Buscar libros por numero de autores',
+    9: 'Editar Libro',
+    10: 'Guardar libros en un nuevo archivo',
+    11: 'Salir'
+}
+
+def mostrar_menu(opciones_del_menu):
+    for key in opciones_del_menu.keys():
+        print(str(key)+'-'+opciones_del_menu[key])
+
+if  __name__ == '__main__':
+    datos_libros = []
+    nombre_archivo =  ''
+    while True:
+        print(' ======================')
+        print('|   MENU PRINCIPAL     |')
+        print(' ======================')
+        mostrar_menu(menu_opciones)        
+        opcion = ''
+        try:
+            opcion = int(input('Ingrese una accion a realizar: '))
+            while opcion not in menu_opciones.keys():
+                opcion = int(input(
+                    'Por favor ingrese una opcion correcta.'))
+        except:
+            print('Por favor escoga un numero como accion a realizar')
+
+        if opcion == 1:
+            print('Cargar archivos')
+            pass
+        elif opcion == 2:
+            print('Listar libros')
+            pass
+        elif opcion == 3:
+            print('Agregar libro')
+            pass
+        elif opcion == 4:
+            print('Eliminar libro')
+            pass
+        elif opcion == 5:
+            print('Buscar libros por isbn o titulo')
+            pass
+        elif opcion == 6:
+            print('ordenar libros por titulo')
+            pass
+        elif opcion == 7:
+            print('Buscar libros por autor, editorial y/o genero')
+            pass
+        elif opcion == 8:
+            print('Buscar libros por numero de autores')
+            pass
+        elif opcion == 9:
+            print('Editar libro')
+            pass    
+        elif opcion == 10:
+            print('Guardar libros en un nuevo archivo')
+            pass
+        elif opcion == 11:
+            print('Salir')
+            exit()
+            pass
+        else:
+            print('Por favor escoga una opcion de las listadas.')
+
+
