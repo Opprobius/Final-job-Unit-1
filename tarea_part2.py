054afd3a907c00c4438c1fa38da00c9ee1c6cddb
FreddyxD5 <freddyxd5@gmail.com>
2022-11-05 00:32:25 -0500

    Menu para API Pokemon añadido

--- tarea_part2.py
+++ tarea_part2.py
@@ -1,21 +1,12 @@
 import requests
-
 #https://pokeapi.co/api/v2
+#Rutas para obtener la informacion
 #generation
 #pokemon-shape
 #ability
 #pokemon-habitat
 #type
 
-#funciones del menu
-menu_options = {
-        1: 'Listar pokemons por generación',
-        2: 'Listar pokemons por forma',
-        3: 'Listar pokemons por habilidad',
-        4: 'Listar pokemons por habitat.',
-        5: 'Listar pokemons por tipo',
-        6: 'Salir del programa.'
-}
 
 #peticiones de Forma
 def obtener_1():
@@ -47,3 +38,53 @@ def obtener_5():
     peticion = requests.get('url')
     if peticion.status_code == 200:
         peticion = peticion.json()
+
+
+
+#FUNCIONES DEL MENU
+menu_opciones = {
+        1: 'Listar pokemons por generación',
+        2: 'Listar pokemons por forma',
+        3: 'Listar pokemons por habilidad',
+        4: 'Listar pokemons por habitat.',
+        5: 'Listar pokemons por tipo',
+        6: 'Salir del programa.'
+}
+
+def mostrar_menu(opciones_menu):
+    for key in opciones_menu.keys():
+        print(str(key)+'-'+opciones_menu[key])
+
+
+
+if __name__ == '__main__':
+    while True:
+        print(' ======================')
+        print('|   MENU PRINCIPAL     |')
+        print(' ======================')
+        mostrar_menu(menu_opciones)
+
+        try:
+            opcion = int(input('Por favor seleccione una opción.'))
+        except:
+            print('Por favor ingrese una opción numerica')
+        if opcion == 1:
+            print('Listar pokemones por generacion')
+        elif opcion == 2:
+            print('Listar pokemones por forma')
+        elif opcion == 3:
+            print('Listar pokemones por habilidades')
+        elif opcion == 4:
+            print('Listar pokemones por habitat')
+        elif opcion == 5:
+            print('Listar pokemones por tipo')
+        elif opcion == 6:
+            print('Listar pokemones por tipo')
+        else:
+            print('Por favor ingrese una opcion correcta.')
+
+    
+
+
+
+
